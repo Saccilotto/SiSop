af216d1682ca009f65a7edb56b59ca6fae0f13d1
Saccilotto <sacilotto.andre@gmail.com>
2024-03-25 15:52:29 -0300

    experimentation version and correct print order sync ex1 version

---
+++ sync/ex1_2mutex_getset.c
@@ -0,0 +1,97 @@
+#define ITERATIONS 100000
+
+#include <pthread.h>
+#include <stdio.h>
+
+struct resource_s {
+	int val;
+	int (*getval)(struct resource_s *);
+	void (*setval)(struct resource_s *, int);
+};
+
+pthread_mutex_t mtx1, mtx2;
+struct resource_s resource;
+
+int getval(struct resource_s *res)
+{
+    pthread_mutex_lock(&mtx1);
+	return res->val;
+    pthread_mutex_unlock(&mtx1);
+}
+
+
+void setval(struct resource_s *res, int nval)
+{
+	pthread_mutex_lock(&mtx2);
+	res->val = nval;
+	pthread_mutex_unlock(&mtx2);
+}
+
+void *task1(void *arg)
+{
+	struct resource_s *ptr_resource = &resource;
+	int val;
+	
+	printf("task 1\n");
+
+	for (int i = 0; i < ITERATIONS; i++) {
+		val = ptr_resource->getval(ptr_resource);
+		ptr_resource->setval(ptr_resource, ++val);
+	}
+}
+
+void *task2(void *arg)
+{
+	struct resource_s *ptr_resource = &resource;
+	int val;
+	
+	printf("task 2\n");
+	
+	for (int i = 0; i < ITERATIONS; i++) {
+		val = ptr_resource->getval(ptr_resource);
+		ptr_resource->setval(ptr_resource, ++val);
+	}
+}
+
+void *task3(void *arg)
+{
+	struct resource_s *ptr_resource = &resource;
+	int val;
+	
+	printf("task 3\n");
+	
+	for (int i = 0; i < ITERATIONS; i++) {
+		val = ptr_resource->getval(ptr_resource);
+		ptr_resource->setval(ptr_resource, ++val);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t threads[3];
+	struct resource_s *ptr_resource = &resource;
+	
+	ptr_resource->val = 0;
+	ptr_resource->getval = getval;
+	ptr_resource->setval = setval;
+
+	pthread_mutex_init(&mtx1, NULL);
+	pthread_mutex_init(&mtx2, NULL);
+
+	pthread_create(&threads[0], NULL, task1, NULL);
+	pthread_create(&threads[1], NULL, task2, NULL);
+	pthread_create(&threads[2], NULL, task3, NULL);
+
+	pthread_join(threads[0], NULL);
+	printf("fim da thread 1\n");
+	pthread_join(threads[1], NULL);
+	printf("fim da thread 2\n");
+	pthread_join(threads[2], NULL);
+	printf("fim da thread 3\n");
+	
+	printf("soma: %d\n", ptr_resource->getval(ptr_resource));
+	
+	pthread_exit(NULL);
+	
+	return 0;
+}
